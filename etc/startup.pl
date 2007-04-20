84ba3ac85f02a0cf8ccb6b6b7cc73e8873b766d8
travail <travail@6c166dde-5619-4943-94b1-efa9d58136dd>
2007-04-20 16:45:17 +0000

    git-svn-id: http://svn.travail.jp/repos/SayCheese/trunk@2 6c166dde-5619-4943-94b1-efa9d58136dd

---
+++ etc/startup.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use lib qw(
+    /home/public/SayCheese/lib
+    /home/public/cgi/lib
+);
+
+use DBIx::Class;
+use Template;
+use DateTime;
+use SayCheese;
+
+use Catalyst;
+
+1;
---
+++ etc/startup_travail.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use lib qw(
+    /home/travail/public_html/SVNHOME/SayCheese/lib
+    /home/public/cgi/lib
+);
+
+use DBIx::Class;
+use Template;
+use DateTime;
+use Apache::DBI;
+
+use Catalyst;
+
+1;
