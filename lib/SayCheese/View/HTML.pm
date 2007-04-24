b24c68b5cde289ac4f692c5e99e8de75428c6d7b
travail <travail@6c166dde-5619-4943-94b1-efa9d58136dd>
2007-04-24 17:44:05 +0000

    git-svn-id: http://svn.travail.jp/repos/SayCheese/trunk@5 6c166dde-5619-4943-94b1-efa9d58136dd

---
+++ lib/SayCheese/View/HTML.pm
@@ -0,0 +1,42 @@
+package SayCheese::View::HTML;
+
+use strict;
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config({
+#    CATALYST_VAR => 'Catalyst',
+    INCLUDE_PATH => [
+        SayCheese->path_to( 'root', 'src' ),
+        SayCheese->path_to( 'root', 'lib' ),
+        SayCheese->path_to( 'root', 'static' ),
+    ],
+#    PRE_PROCESS  => 'config/main',
+#    WRAPPER      => 'site/wrapper',
+#    ERROR        => 'error.tt2',
+#    TIMER        => 0
+});
+
+=head1 NAME
+
+SayCheese::View::HTML - TT View for SayCheese
+
+=head1 DESCRIPTION
+
+TT View for SayCheese. 
+
+=head1 AUTHOR
+
+=head1 SEE ALSO
+
+L<SayCheese>
+
+A clever guy
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
