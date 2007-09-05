dc5025f0e07f6212f03e7c7325fd41ffc6850691
root <root@6c166dde-5619-4943-94b1-efa9d58136dd>
2007-09-05 14:47:52 +0000

    git-svn-id: http://svn.travail.jp/repos/SayCheese/trunk@71 6c166dde-5619-4943-94b1-efa9d58136dd

--- root/static/js/saycheese.js
+++ root/static/js/saycheese.js
@@ -36,7 +36,7 @@ function Thumbnail(url) {
 
   new Ajax.Request('/ajaxrequest/thumbnail/create', {
     method: 'get',
-    parameters:    '&url=' + url,
+    parameters:    '&url=' + encodeURIComponet(url),
     onCreated:     ShowIndicator(),
     onAccepted:    ShowIndicator(),
     onLoading:     ShowIndicator(),
