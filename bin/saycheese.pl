c2415fa03337b9e33a1dab333294c5798ff9998f
(no author) <(no author)@6c166dde-5619-4943-94b1-efa9d58136dd>
2007-10-08 14:46:56 +0000

    git-svn-id: http://svn.travail.jp/repos/SayCheese/trunk@130 6c166dde-5619-4943-94b1-efa9d58136dd

--- bin/saycheese.pl
+++ bin/saycheese.pl
@@ -52,7 +52,7 @@ $worker->register_function(
             warn "OK : $url exists.\n";
         } else {
             warn sprintf qq{ERROR : %s.\n}, $res->status_line;
-            warn "ERROR : $url does not exist.\n\n";
+            warn "ERROR : $url does not exist.\n";
             warn "FINISH saycheese.pl\n\n";
             return;
         }
