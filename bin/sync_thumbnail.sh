6c6e8362a5836f157af969dbd01dd0fc485047e6
(no author) <(no author)@6c166dde-5619-4943-94b1-efa9d58136dd>
2008-10-17 13:01:16 +0000

    git-svn-id: http://svn.travail.jp/repos/SayCheese/trunk@262 6c166dde-5619-4943-94b1-efa9d58136dd

--- bin/sync_thumbnail.sh
+++ bin/sync_thumbnail.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rsync -avh --dry-run --progress /home/public/var/ 192.168.1.2:/home/public/var/
+rsync -avh --dry-run --progress /home/public/var/SayCheese/thumbnail/ 192.168.1.2:/home/public/var/SayCheese/thumbnail/
