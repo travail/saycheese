3ef9d6b05859ca524cb47b2e64795e8edbaa541a
travail <travail@hibinokoto.jp>
2010-05-12 12:09:00 +0900

    Added namespace fetchtitled, updatetitled.

--- etc/conf/deploy/prod.rb
+++ etc/conf/deploy/prod.rb
@@ -22,6 +22,34 @@ namespace :deploy do
       run "#{sudo} svc -t /service/saycheesed && #{sudo} svc -t /service/saycheesed/log"
     end
   end
+  namespace :fetchtitled do
+    desc 'Start fetchtitled'
+    task :start do
+      run "#{sudo} mv /service/.fetchtitled /service/fetchtitled"
+    end
+    desc 'Stop fetchtitled'
+    task :stop do
+      run "#{sudo} mv /service/fetchtitled /service/.fetchtitled && #{sudo} svc -dx /service/.fetchtitled && #{sudo} svc -dx /service/.fetchtitled/log"
+    end
+    desc 'Restart fetchtitled'
+    task :restart do
+      run "#{sudo} svc -t /service/fetchtitled && #{sudo} svc -t /service/fetchtitled/log"
+    end
+  end
+  namespace :updatetitled do
+    desc 'Start updatetitled'
+    task :start do
+      run "#{sudo} mv /service/.updatetitled /service/updatetitled"
+    end
+    desc 'Stop updatetitled'
+    task :stop do
+      run "#{sudo} mv /service/updatetitled /service/.updatetitled && #{sudo} svc -dx /service/.updatetitled && #{sudo} svc -dx /service/.updatetitled/log"
+    end
+    desc 'Restart fetchtitled'
+    task :restart do
+      run "#{sudo} svc -t /service/updatetitled && #{sudo} svc -t /service/updatetitled/log"
+    end
+  end
 
   desc 'Restart apache'
   task :restart do
