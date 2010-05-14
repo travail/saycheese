257b695df03c1ddbf3f42bdcedfe31d90051d562
travail <travail@hibinokoto.jp>
2010-05-14 11:43:28 +0900

    Trivial changes.

--- bin/queue/saycheese/reenqueue.pl
+++ bin/queue/saycheese/reenqueue.pl
@@ -4,6 +4,6 @@ use strict;
 use warnings;
 use FindBin ();
 use lib "$FindBin::Bin/../../../lib";
-use SayCheese::CLI::Queue::SayCheese::Reenqueue
+use SayCheese::CLI::Queue::SayCheese::Reenqueue;
 
 SayCheese::CLI::Queue::SayCheese::Reenqueue->new_with_options->run;
