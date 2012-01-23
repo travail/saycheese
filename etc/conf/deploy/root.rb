178957fb49c8bf583c3d9b563ae75d719e5914c5
travail <sawa.tomoyuki@gmail.com>
2012-01-23 22:52:58 +0900

    Modified capistrano configuration files. #218: Place httpd.conf in /etc/httpd/conf.d by capistrano

--- etc/conf/deploy/root.rb
+++ etc/conf/deploy/root.rb
@@ -1,17 +1,17 @@
-set :application, 'SayCheese'
+set :application, "SayCheese"
 set :use_sudo,    false
 set :scm, :git
-set :repository,  'ssh://git.travail.jp/var/git/saycheese.git'
-set :branche,     'master'
+set :repository,  "ssh://git.travail.jp/var/git/saycheese.git"
+set :branche,     "master"
 set :deploy_via,  :export
 set :shared_children, %w(log root/src/include)
 
 namespace :deploy do
-  desc 'Setup Catalyst application'
+  desc "Setup Catalyst application"
   task :setup, :roles => [:web, :app] do
     dirs = [deploy_to, releases_path, shared_path]
     dirs += shared_children.map { |d| File.join(shared_path, d) }
-    run "mkdir -p #{dirs.join(' ')} && chmod g+w #{dirs.join(' ')}"
+    run "mkdir -p #{dirs.join(" ")} && chmod g+w #{dirs.join(" ")}"
     run "#{sudo} chmod -R g+w #{shared_path}/root"
     run "mkdir -p #{shared_path}/cache"
     run "mkdir -p #{shared_path}/cache/tt"
@@ -20,58 +20,58 @@ namespace :deploy do
   end
 
   # finalize update
-  desc 'Finalize update'
+  desc "Finalize update"
   task :finalize_update, roles => [:web, :app] do
   end
 
   # worker
   namespace :saycheesed do
-    desc 'Start saycheesed'
+    desc "Start saycheesed"
     task :start do
       daemon_start "#{saycheesed_name}"
     end
-    desc 'Stop saycheesed'
+    desc "Stop saycheesed"
     task :stop do
       daemon_stop "#{saycheesed_name}"
     end
-    desc 'Restart saycheesed'
+    desc "Restart saycheesed"
     task :restart do
       daemon_restart "#{saycheesed_name}"
     end
   end
 
   namespace :fetchtitled do
-    desc 'Start fetchtitled'
+    desc "Start fetchtitled"
     task :start do
       daemon_start "#{fetchtitled_name}"
     end
-    desc 'Stop fetchtitled'
+    desc "Stop fetchtitled"
     task :stop do
       daemon_stop "#{fetchtitled_name}"
     end
-    desc 'Restart fetchtitled'
+    desc "Restart fetchtitled"
     task :restart do
       daemon_restart "#{fetchtitled_name}"
     end
   end
 
   namespace :updatetitled do
-    desc 'Start updatetitled'
+    desc "Start updatetitled"
     task :start do
       daemon_start "#{updatetitled_name}"
     end
-    desc 'Stop updatetitled'
+    desc "Stop updatetitled"
     task :stop do
       daemon_stop "#{updatetitled_name}"
     end
-    desc 'Restart updatetitled'
+    desc "Restart updatetitled"
     task :restart do
       daemon_restart "#{updatetitled_name}"
     end
   end
 
   # httpd
-  desc 'Restart apache'
+  desc "Restart apache"
   task :restart do
     parallel do |session|
       session.when "in?(:web) || in?(:app)", "#{sudo} /etc/rc.d/init.d/httpd restart"
@@ -80,7 +80,7 @@ namespace :deploy do
   end
 
   namespace :web do
-    desc ''
+    desc ""
     task :enable, :roles => :web do
       run <<-CMD
         #{sudo} rm -f #{httpd_conf_root}/web.conf &&
@@ -88,7 +88,7 @@ namespace :deploy do
         #{sudo} /etc/rc.d/init.d/httpd reload
       CMD
     end
-    desc ''
+    desc ""
     task :disable, :roles => :web do
       run <<-CMD
         #{sudo} rm -f #{httpd_conf_root}/web.conf &&
@@ -99,7 +99,7 @@ namespace :deploy do
   end
 
   namespace :thumb do
-    desc ''
+    desc ""
     task :enable, :roles => :web do
       run <<-CMD
         #{sudo} rm -f #{httpd_conf_root}/thumb.conf &&
@@ -107,11 +107,29 @@ namespace :deploy do
         #{sudo} /etc/rc.d/init.d/httpd reload
       CMD
     end
-    desc ''
+    desc ""
     task :disable, :roles => :web do
       run <<-CMD
         #{sudo} rm -f #{httpd_conf_root}/thumb.conf &&
         #{sudo} ln -s #{current_path}/etc/httpd/#{thumb_maintenance_conf_file} #{httpd_conf_root}/thumb.conf &&
+        #{sudo} /etc/rc.d/init.d/httpd reload
+      CMD
+    end
+  end
+
+  namespace :app do
+    desc ""
+    task :enable, :roles => :app do
+      run <<-CMD
+        #{sudo} rm -f #{httpd_conf_root}/app.conf &&
+        #{sudo} ln -s #{current_path}/etc/httpd/#{app_conf_file} #{httpd_conf_root}/app.conf &&
+        #{sudo} /etc/rc.d/init.d/httpd reload
+      CMD
+    end
+    desc ""
+    task :disable, :roles => :app do
+      run <<-CMD
+        #{sudo} rm -f #{httpd_conf_root}/app.conf &&
         #{sudo} /etc/rc.d/init.d/httpd reload
       CMD
     end
